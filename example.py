c303f63ffd07cdfbd7ae4eed7747bca19ba63330
tarnacious <tarn@tarnbarford.net>
2018-06-14 01:17:02 +0200

    Simpify example

--- example.py
+++ example.py
@@ -5,27 +5,15 @@ from rratelimit import Limiter
 
 loop = asyncio.get_event_loop()
 
-async def get_pool():
-    return await aioredis.create_redis_pool(
-        'redis://localhost',
-        minsize=5, maxsize=10,
-        loop=loop)
-
-pool = loop.run_until_complete(get_pool())
-
-limit = 2
-period = 3
-key = 'request_{}_{}'.format(limit, period)
-
-async def get_limiter():
-    limiter =  Limiter(pool, action=key, limit=limit, period=period)
+async def go():
+    redis = await aioredis.create_redis_pool('redis://localhost')
+    key = "user_x_bla"
+    limit = 2
+    period = 3
+    limiter = Limiter(redis, action=key, limit=limit, period=period)
     await limiter.setup()
-    return limiter
+    for _ in range(5):
+        print(await limiter.checked_insert(key))
 
-async def check_limit(limiter, key):
-    return await limiter.checked_insert(key)
+loop.run_until_complete(go())
 
-limiter = loop.run_until_complete(get_limiter())
-
-for _ in range(5):
-    print(loop.run_until_complete(check_limit(limiter, key)))
